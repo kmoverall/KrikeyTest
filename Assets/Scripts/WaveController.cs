774a56d57de2f497b9060dd48b9d974a5ebb59fd
KevinOverall <kevin@monkeymindinteractive.com>
2023-09-19 16:49:28 -0400

    Implemented Enemy Movement

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -18,6 +18,53 @@ public class WaveController : MonoBehaviour
     [SerializeField]
     private float _SpeedPerKill;
 
+    [SerializeField]
+    private float _XLimit;
+
+    [SerializeField]
+    private float _StepdownDistance;
+
+    private bool _MovingRight = true;
+
+    // Gets the position of the furthest enemies still alive
+    private Bounds EnemyBounds {
+        get {
+            Vector3 min = Vector3.zero;
+            Vector3 max = Vector3.zero;
+            Bounds result = new Bounds();
+            IEnumerable<Enemy> check;
+
+            for (int i = 0; i < Enemies[0].Count; i++)
+            {
+                check = Enemies.Select(r => r[i]);
+                if (check.Any(e => e.gameObject.activeSelf))
+                {
+                    max.x = check.First().transform.localPosition.x;
+                    break;
+                }
+            }
+            for (int i = Enemies[0].Count-1; i >=0; i--)
+            {
+                check = Enemies.Select(r => r[i]);
+                if (check.Any(e => e.gameObject.activeSelf))
+                {
+                    min.x = check.First().transform.localPosition.x;
+                    break;
+                }
+            }
+
+            var top = Enemies.First(e => e.Any(e => e.gameObject.activeSelf));
+            max.y = top.First().transform.localPosition.y;
+
+            var bottom = Enemies.Last(e => e.Any(e => e.gameObject.activeSelf));
+            min.y = bottom.First().transform.localPosition.y;
+
+            result.min = min;
+            result.max = max;
+            return result;
+        }
+    }
+
     public void Initialize()
     {
         foreach (var row in Enemies)
@@ -33,13 +80,38 @@ public class WaveController : MonoBehaviour
 
     private void Update()
     {
+        var sign = _MovingRight ? -1 : 1;
+
+        var position = transform.position;
+        // EnemyBounds is expensive, so we're making sure its only called once per Update
+        var bounds = EnemyBounds;
+
+        position.x += sign * _InitialSpeed * _SpeedMultiplier * Time.deltaTime;
+
+        if (position.x + bounds.max.x > _XLimit)
+        {
+            Debug.Log("Hit Max");
+            position.x = _XLimit - bounds.max.x;
+            position.y -= _StepdownDistance;
+            _MovingRight = !_MovingRight;
+        }
+
+        if (position.x + bounds.min.x < -1 * _XLimit)
+        {
+            Debug.Log("Hit Min");
+            position.x = -1 * _XLimit - bounds.min.x;
+            position.y -= _StepdownDistance;
+            _MovingRight = !_MovingRight;
+        }
+
+        transform.position = position;
     }
 
     private void OnDestroy(Enemy target)
     {
         if (target.Column > 0)
         {
-            Enemies[target.Row][target.Column].CanAttack = true;
+            Enemies[target.Row-1][target.Column].CanAttack = true;
         }
         _SpeedMultiplier += _SpeedPerKill;
         Enemies.ForEach(r => r.ForEach(e => e.SpeedMultiplier = _SpeedMultiplier));
